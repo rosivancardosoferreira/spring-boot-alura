8619761a2d5c4617ae6b706716a7e920a9de3720
rosivancardoso <rosivancardoso777@outlook.com>
2023-03-11 15:38:36 -0400

    chore: created project

---
+++ src/test/java/br/com/rosivan/RestWithSpringBootAndJavaRosivanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rosivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaRosivanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
