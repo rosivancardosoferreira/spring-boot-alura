f515c7f11f2d6ecbb9b8d99d740f2d6d23345af5
rosivancardoso <rosivancardoso777@outlook.com>
2023-07-01 11:57:21 -0400

    configuração do retorno da api

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (2, 'Carlos Castelo Branco', 'Rosivan', 'Masc', 'Cardoso'),
+    (6, 'Carlos Castelo Branco', 'Leonardo', 'Mo', 'Da Vinci'),
+    (7, 'Carlos Castelo Branco', 'Leonardo moto', 'Mo', 'Da Vinci'),
+    (8, 'Carlos Castelo Branco', 'MOCK BIRDTHDATA', 'Mo', 'Da Vinci');
